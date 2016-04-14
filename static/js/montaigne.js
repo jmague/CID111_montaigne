335f85317b06dde183a217377ccb4baea1b2d3d2
Jean-Philippe Magué <jean-philippe.mague@ens-lyon.fr>
2016-04-14 15:00:15 +0200

    afficher les auteurs des rapports

--- static/js/montaigne.js
+++ static/js/montaigne.js
@@ -24,7 +24,7 @@ function show(edition){
 function addBrackets(edition){
   var variantes = document.getElementsByClassName('wit_'+edition);
   for (var i = 0; i < variantes.length; i ++) {
-    if variantes[i].tagName=='span'{
+    if (variantes[i].tagName=='SPAN'){
         variantes[i].setAttribute('data-before','[');
         variantes[i].setAttribute('data-after',']');
       }
